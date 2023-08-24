24092e9461348e187883de2528a32a069255d6e2
Rafael Assumpção <102435416+ProfessorRafaelAssumpcao@users.noreply.github.com>
2023-08-24 09:56:58 -0300

    add arquivos .js & .css

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background("red");
+    fill("white");
+    textSize(12);
+    textAlign (CENTER, CENTER);
+    
+    let máximo = width;
+    let mínimo = 0;
+    
+    let palavra = "sentimento que jamais acabará";
+    let quantidade = map(mouseX,0,width,1,palavra.length);
+    
+    let parcial = palavra.substring (0,quantidade);
+    text(parcial, 200,200);
+    
+    
+  }
